f5c9f1ba346743aa35031bb9463984338fb46a23
Juan Pablo <juanpan2409@icloud.com>
2023-02-21 11:59:35 -0400

    crawl single page eith error hnadling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,25 @@
 const {JSDOM} = require("jsdom");
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling ${currentURL}`);
+    try{
+        const resp = await fetch(currentURL);
+        if(resp.status>399){
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`);
+            return
+        }
+        const contentType = resp.headers.get('content-type');
+        if(!contentType.includes("text/html")){
+            console.log(`none html response, content type: ${contentType}, on page: ${currentURL}`);
+            return
+        }
+        console.log( await resp.text());
+
+    }catch(e){
+        console.log(`error fetching ${currentURL}: ${e}`);
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL){
     const urls=[];
     const dom = new JSDOM(htmlBody);
@@ -36,6 +56,7 @@ function normalizeURL(urlString){
 }
 module.exports={
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
 
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const {crawlPage} = require("./crawl.js");
+function main(){
+    if(process.argv.length<3){
+        console.log("no website provided");
+        process.exit(1);
+    }
+    if(process.argv.length>3){
+        console.log("too many commando line args");
+        process.exit(1);
+    }
+    for( const arg of process.argv){
+        console.log(arg);
+    }
+    const baseURL = process.argv[2];
+    console.log(`staring crawl of ${baseURL}`);
+    crawlPage(baseURL);
+}
+main();
