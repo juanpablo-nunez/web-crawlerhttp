dbbaa36e376463af42856dbe2473da3032f445ff
Juan Pablo <juanpan2409@icloud.com>
2023-02-21 12:26:18 -0400

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,23 +1,39 @@
 const {JSDOM} = require("jsdom");
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
+    const baseURLObje = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if(baseURLObje.hostname !== currentURLObj.hostname){
+        return pages;
+    }
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if(pages[normalizedCurrentURL]> 0){
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+    pages[normalizedCurrentURL] = 1;
     console.log(`actively crawling ${currentURL}`);
+    let htmlBody = ''
     try{
         const resp = await fetch(currentURL);
         if(resp.status>399){
             console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`);
-            return
+            return pages
         }
         const contentType = resp.headers.get('content-type');
         if(!contentType.includes("text/html")){
             console.log(`none html response, content type: ${contentType}, on page: ${currentURL}`);
-            return
+            return pages
         }
-        console.log( await resp.text());
-
+        htmlBody = await resp.text();
     }catch(e){
         console.log(`error fetching ${currentURL}: ${e}`);
     }
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    for(const nextURL of nextURLs){
+       pages = await crawlPage(baseURL, nextURL, pages);
+    }
+    return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
